28e08914b0fc4770a4d1c2e9d35533e26dd7607c
Niklas Bergström <niklas@dyna218-220.nada.kth.se>
2012-03-20 16:27:04 +0100

    Initial Commit

---
+++ wallfinder/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  wallfinder
+//
+//  Created by Niklas Bergström on 3/20/12.
+//  Copyright (c) 2012 KTH. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
